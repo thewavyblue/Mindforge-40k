ba1f8f9ed79ea112947c6aa5f402ab3b4981fdcd
Ben Hoath <thewavyblue@gmail.com>
2024-03-28 22:45:42 +0000

    v1.0.1 Updated entire Sister of Battle Index to the database

--- armyStats.js
+++ armyStats.js
@@ -1,6 +1,39 @@
 export const armyStats = {
     armyName: "Sisters of Battle",
     unit1: {
+        unitName: "Aestred Thurga",
+        stats: {
+            move: 6,
+            toughness: 3,
+            save: 2,
+            invulnSave: 4,
+            wounds: 4,
+            leadership: 6,
+            oc: 1
+        }},
+    unit2: {
+        unitName: "Agathae Dolan",
+        stats: {
+            move: 6,
+            toughness: 3,
+            save: 6,
+            invulnSave: 4,
+            wounds: 3,
+            leadership: 7,
+            oc: 1
+        }},
+    unit3: {
+        unitName: "Arco-Flagellants",
+        stats: {
+            move: 7,
+            toughness: 3,
+            save: 7,
+            invulnSave: 0,
+            wounds: 2,
+            leadership: 8,
+            oc: 1
+        }},
+    unit4: {
         unitName: "Battle Sister Squad",
         stats: {
             move: 6,
@@ -11,26 +44,367 @@ export const armyStats = {
             leadership: 7,
             oc: 2
         }},
-    unit2: {
-        unitName: "Sisters Repentia",
+    unit5: {
+        unitName: "Canoness",
         stats: {
-            move: 7,
+            move: 6,
             toughness: 3,
-            save: 7,
-            invulnSave: 6,
-            wounds: 1,
-            leadership: 8,
+            save: 3,
+            invulnSave: 4,
+            wounds: 4,
+            leadership: 7,
             oc: 1
         }},
-    unit3: {
+    unit6: {
+        unitName: "Castigator",
+        stats: {
+            move: 10,
+            toughness: 10,
+            save: 3,
+            invulnSave: 6,
+            wounds: 11,
+            leadership: 7,
+            oc: 3
+        }},
+    unit7: {
+        unitName: "Celestian Sacresants",
+        stats: {
+            move: 6,
+            toughness: 3,
+            save: 3,
+            invulnSave: 4,
+            wounds: 1,
+            leadership: 7,
+            oc: 1
+        }},
+    unit8: {
+        unitName: "Crusaders",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       3,
+            invulnSave: 4,
+            wounds:     1,
+            leadership: 7,
+            oc:         1
+        }},
+    unit9: {
+        unitName: "Daemonifuge: Ephrael Stern",
+        stats: {
+            move:       8,
+            toughness:  3,
+            save:       3,
+            invulnSave: 4,
+            wounds:     5,
+            leadership: 7,
+            oc:         1
+        }},
+    unit10: {
+        unitName: "Daemonifuge: Kyganil of the Bloody Tears",
+        stats: {
+            move:       8,
+            toughness:  3,
+            save:       6,
+            invulnSave: 4,
+            wounds:     3,
+            leadership: 7,
+            oc:         1
+        }},
+    unit11: {
+        unitName: "Death Cult Assassins",
+        stats: {
+            move:       7,
+            toughness:  3,
+            save:       5,
+            invulnSave: 5,
+            wounds:     1,
+            leadership: 7,
+            oc:         1
+        }},
+    unit12: {
+        unitName: "Dialogus",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       3,
+            invulnSave: 6,
+            wounds:     3,
+            leadership: 6,
+            oc:         1
+        }},
+    unit13: {
+        unitName: "Dogmata",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       3,
+            invulnSave: 6,
+            wounds:     3,
+            leadership: 6,
+            oc:         1
+        }},
+    unit14: {
+        unitName: "Dominion Squad",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       3,
+            invulnSave: 6,
+            wounds:     1,
+            leadership: 7,
+            oc:         1
+        }},
+    unit15: {
+        unitName: "Exorcist",
+        stats: {
+            move:       10,
+            toughness:  10,
+            save:       3,
+            invulnSave: 6,
+            wounds:     11,
+            leadership: 7,
+            oc:         3
+        }},
+    unit16: {
+        unitName: "Hospitaller",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       3,
+            invulnSave: 6,
+            wounds:     3,
+            leadership: 7,
+            oc:         1
+        }},
+    unit17: {
+        unitName: "Imagifier",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       3,
+            invulnSave: 4,
+            wounds:     3,
+            leadership: 6,
+            oc:         1
+        }},
+    unit18: {
+        unitName: "Immolator",
+        stats: {
+            move:       12,
+            toughness:  10,
+            save:       3,
+            invulnSave: 6,
+            wounds:     11,
+            leadership: 7,
+            oc:         2
+        }},
+    unit19: {
+        unitName: "Junith Eruita",
+        stats: {
+            move:       6,
+            toughness:  4,
+            save:       2,
+            invulnSave: 4,
+            wounds:     6,
+            leadership: 6,
+            oc:         2
+        }},
+    unit20: {
+        unitName: "Missionary",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       6,
+            invulnSave: 4,
+            wounds:     3,
+            leadership: 7,
+            oc:         1
+        }},
+    unit21: {
+        unitName: "Mortifiers",
+        stats: {
+            move:       8,
+            toughness:  6,
+            save:       4,
+            invulnSave: 6,
+            wounds:     5,
+            leadership: 7,
+            oc:         2
+        }},
+    unit22: {
+        unitName: "Morvenn Vahl",
+        stats: {
+            move:       8,
+            toughness:  6,
+            save:       2,
+            invulnSave: 4,
+            wounds:     8,
+            leadership: 6,
+            oc:         2
+        }},
+    unit23: {
+        unitName: "Palatine",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       3,
+            invulnSave: 4,
+            wounds:     3,
+            leadership: 7,
+            oc:         1
+        }},
+    unit24: {
+        unitName: "Paragon Warsuits",
+        stats: {
+            move:       8,
+            toughness:  6,
+            save:       2,
+            invulnSave: 4,
+            wounds:     4,
+            leadership: 7,
+            oc:         2
+        }},
+    unit25: {
         unitName: "Penitent Engines",
         stats: {
-            move: 8,
-            toughness: 6,
-            save: 4,
+            move:       8,
+            toughness:  6,
+            save:       4,
             invulnSave: 0,
-            wounds: 5,
+            wounds:     5,
             leadership: 7,
-            oc: 2
-        }}
+            oc:         2
+        }},
+    unit26: {
+        unitName: "Preacher",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       6,
+            invulnSave: 4,
+            wounds:     3,
+            leadership: 7,
+            oc:         1
+        }},
+    unit27: {
+        unitName: "Repentia Superior",
+        stats: {
+            move:       7,
+            toughness:  3,
+            save:       3,
+            invulnSave: 6,
+            wounds:     1,
+            leadership: 7,
+            oc:         1
+        }},
+    unit28: {
+        unitName: "Sister Repentia",
+        stats: {
+            move:       7,
+            toughness:  3,
+            save:       7,
+            invulnSave: 6,
+            wounds:     1,
+            leadership: 8,
+            oc:         1
+        }},
+    unit29: {
+        unitName: "Retributor Squad",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       3,
+            invulnSave: 6,
+            wounds:     1,
+            leadership: 7,
+            oc:         1
+        }},
+    unit30: {
+        unitName: "Saint Celestine",
+        stats: {
+            move:       12,
+            toughness:  3,
+            save:       2,
+            invulnSave: 4,
+            wounds:     5,
+            leadership: 6,
+            oc:         1
+        }},
+    unit31: {
+        unitName: "Geminae Superia",
+        stats: {
+            move:       12,
+            toughness:  3,
+            save:       2,
+            invulnSave: 4,
+            wounds:     2,
+            leadership: 7,
+            oc:         1
+        }},
+    unit32: {
+        unitName: "Seraphim Squad",
+        stats: {
+            move:       12,
+            toughness:  3,
+            save:       3,
+            invulnSave: 5,
+            wounds:     1,
+            leadership: 7,
+            oc:         1
+        }},
+    unit33: {
+        unitName: "Novitiate Superior",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       3,
+            invulnSave: 6,
+            wounds:     1,
+            leadership: 7,
+            oc:         2
+        }},
+    unit34: {
+        unitName: "Sister Novitiate",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       4,
+            invulnSave: 6,
+            wounds:     1,
+            leadership: 8,
+            oc:         2
+        }},
+    unit35: {
+        unitName: "Sororitas Rhino",
+        stats: {
+            move:       12,
+            toughness:  9,
+            save:       3,
+            invulnSave: 6,
+            wounds:     10,
+            leadership: 7,
+            oc:         2
+        }},
+    unit36: {
+        unitName: "Triumph of Saint Katherine",
+        stats: {
+            move:       6,
+            toughness:  3,
+            save:       3,
+            invulnSave: 4,
+            wounds:     18,
+            leadership: 6,
+            oc:         6
+        }},
+    unit37: {
+        unitName: "Zephyrim Squad",
+        stats: {
+            move:       12,
+            toughness:  3,
+            save:       3,
+            invulnSave: 5,
+            wounds:     1,
+            leadership: 7,
+            oc:         1
+        }},
     };
