2b328f07cfdd5c15e113e4c3c6b72127695c227a
Ben <thewavyblue@gmail.com>
2024-04-17 20:03:00 +0100

    v.1.2.6 Started on quotes.js and game-over.html.

---
+++ quotes.js
@@ -0,0 +1,109 @@
+export const quotes = [
+    {
+        quote: "It’s amazing how much paperwork an act of heroism requires.",
+        credit: "Ciaphas Cain"
+    },
+    {
+        quote: "No pity! No remorse! No fear!",
+        credit: "Chaplain Grimaldus"
+    },
+    {
+        quote: "Let the galaxy burn!",
+        credit: "Abaddon the Despoiler"
+    },
+    {
+        quote: "Kill! Maim! Burn!",
+        credit: "Khârn the Betrayer"
+    },
+    {
+        quote: "The most dangerous weapon is the one no one expects.",
+        credit: "Inquisitor Gregor Eisenhorn"
+    },
+    {
+        quote: "There is no art more beautiful and diverse as the art of death.",
+        credit: "Farseer Macha"
+    },
+    {
+        quote: "To be a man in such times is to be one amongst untold billions. It is to live in the cruellest and most bloody regime imaginable.",
+        credit: "Narrator"
+    },
+    {
+        quote: "I’m da hand of Gork and Mork, dey sent me to make war!",
+        credit: "Ghazghkull Mag Uruk Thraka"
+    },
+    {
+        quote: "A mind without purpose will wander in dark places.",
+        credit: "Ravenor"
+    },
+    {
+        quote: "Victory is achieved through mettle. Glory is achieved through metal.",
+        credit: "Iron Hands Maxim"
+    },
+    {
+        quote: "A hero is no braver than an ordinary man, but he is brave five minutes longer.",
+        credit: "Rogal Dorn"
+    },
+    {
+        quote: "",
+        credit: ""
+    },
+    {
+        quote: "",
+        credit: ""
+    },
+    {
+        quote: "",
+        credit: ""
+    },
+    {
+        quote: "",
+        credit: ""
+    },
+    {
+        quote: "",
+        credit: ""
+    },
+    {
+        quote: "",
+        credit: ""
+    },
+    {
+        quote: "",
+        credit: ""
+    },
+] 
+
+ – 
+ – 
+ – 
+ – 
+ – 
+ – 
+ – 
+ – 
+ – 
+
+ – 
+ – 
+They shall be my finest warriors, these men who give of themselves to me. – The Emperor of Mankind
+Suffer not the unclean to live. – Puritan Inquisitor
+I have dug my grave in this place and I will either triumph or I will die! – Commissar Yarrick
+We do not fight for victory, but for the survival of our species. – Farseer Eldrad Ulthran
+By His will alone is Terra kept safe. – Adeptus Custodes Creed
+Ruthlessness is the kindness of the wise. – Lord Solar Macharius
+A small mind is easily filled with faith. – Inquisitor Gideon Ravenor
+
+Only the insane have strength enough to prosper. Only those who prosper may truly judge what is sane. – Night Lords
+For those who seek perfection, there can be no rest on this side of the grave. – Adeptus Mechanicus Belief
+Beginning reform is beginning a revolution. – Inquisition Teaching
+Success is measured in blood, yours or your enemy´s. – Khorne Worshipper Saying
+The blood of martyrs is the seed of the Imperium. – Ecclesiarchy Saying
+Truth is Subjective. – Inquisition Maxim
+We are the Emperor’s angels of death, not his angels of mercy. – Blood Ravens Devastator Sergeant Avitus
+
+What I cannot crush with words, I will crush with the tanks of the Imperial Guard! – Lord Commander Solar Macharius
+I fear no evil, for I am fear incarnate! – Chaos Space Marines
+Embrace the Immaterium. – Chaos Sorcerer
+Victory is but a prelude to the next battle. – Space Marine Commander
+The enemy of my enemy is still my enemy. – Ork Units
+Flesh is weak! – Techmarine
