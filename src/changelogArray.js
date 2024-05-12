2d26de5dad9e90d158a1330fb0271ee57f2ec627
Ben <thewavyblue@gmail.com>
2024-05-12 14:06:51 +0100

    v1.7.7 Made a few UI changes to the game play screens. Added the rest of the development entries to the changelog.

--- src/changelogArray.js
+++ src/changelogArray.js
@@ -20,8 +20,202 @@ export const changelogArray = [
         description : "Added Astra Militarum army unit stats."
     },
     {
-        version : "v1.0",
+        version : "v1.6.3.1",
+        description : "Minor update"
+    },
+    {
+        version : "v1.6.3",
+        description : "Updated T'au JSON to included fully updated rules."
+    },
+    {
+        version : "v1.6.2",
+        description : "Further file and code cleanup"
+    },
+    {
+        version : "v1.6.1",
+        description : "Quick fix on the Timed Mode game setup button."
+    },
+    {
+        version : "v1.6",
+        description : `Big improvements to the app:<br>
+        - Learn to play page<br>
+        - fixed functionality on game setup page<br>
+        - both game types 'free play' and 'timed mode' working correctly and consistently.`
+    },
+    {
+        version : "v1.5.1",
+        description : "Folder cleanup. Ready to start working on loading the weapon testing version of the game."
+    },
+    {
+        version : "v1.5",
+        description : "Fixed the bug to now accept JSON data."
+    },
+    {
+        version : "v1.4.2",
+        description : "Queried to Discord community"
+    },
+    {
+        version : "v1.4.1",
+        description : "Refactored some code in timedMode.js. About to start finding and smushing the bug"
+    },
+    {
+        version : "v1.4",
+        description : "Installed npm and node in order to us ESLint"
+    },
+    {
+        version : "v1.3.8",
+        description : "More clarity added to file by way of 'better comments'"
+    },
+    {
+        version : "v1.3.7",
+        description : "Cleaned up code in timedMode.js for review"
+    },
+    {
+        version : "v1.3.6",
+        description : "Minor CSS changes"
+    },
+    {
+        version : "v1.3.5",
+        description : "Game Over screen now appears at the end of the game with correct score"
+    },
+    {
+        version : "v.1.3.4",
+        description : "Gave up on multi-page app route as I just couldnt get it to work. I've concentrated on importing army data via JSON, which is all working save for the random unit generation."
+    },
+    {
+        version : "v1.3.3",
+        description : "Trying to dynamically load content into a loaded html page (app.html) and I'm going nowhere fast. I think its to do with the asynchronosity of the app loading things, but I just can't quite find the answer, though I know I'm very close because my test code shows that the code is working."
+    },
+    {
+        version : "v1.3.2",
+        description : "Selector now disables unavailable options"
+    },
+    {
+        version : "v1.3.1",
+        description : "Building the selection dropdown options to actually select an army to play with"
+    },
+
+    {
+        version : "v1.3",
+        description : "Using JSON to implement armyData. Working 'game-over' page with updating score of that round played"
+    },
+    {
+        version : "v1.2.13",
+        description : "Making the Free Play and Timed Modes UI consistent."
+    },
+    {
+        version : "v1.2.12",
+        description : "Commit of working app for demo purposes"
+    },
+    {
+        version : "v1.2.11",
         description : "Using JSON to implement armyData"
     },
+    {
+        version : "v1.2.10",
+        description : "File organisation"
+    },
+    {
+        version : "v1.2.9",
+        description : "Fixed background color height not filling the screen at all times."
+    },
+    {
+        version : "v1.2.8",
+        description : "Amended timer length to 30secs"
+    },
+    {
+        version : "v1.2.7",
+        description : "Implemented game over screen, but final score isnt working yet."
+    },
+    {
+        version : "v1.2.6",
+        description : "Started on quotes.js and game-over.html."
+    },
+    {
+        version : "v1.2.5",
+        description : "Made a few link corrections and styling adjustments to use as a base-line for the rest of this session's development."
+    },
+    {
+        version : "v1.2.4",
+        description : "Score UI has changed dramatically, incorporating colour into the feedback, and further thinking into the space used up by a mobile on-screen keyboard."
+    },
+    {
+        version : "v1.2.3",
+        description : "Timer is working, and as a separate imported js module!"
+    },
+    {
+        version : "v1.2.2",
+        description : "Working out the timer in timed mode."
+    },
+    {
+        version : "v1.2.1.1",
+        description : "Resending commit"
+    },
+    {
+        version : "v1.2.1",
+        description : "Updated UI"
+    },
+    {
+        version : "v1.2.0",
+        description : `Quite a lot has changed here, so I've upgraded the minor version control to 2.<br>
+        - homepage now has a slider card carousel to pick a mode. It doesn't quite wrap around to the start yet, which I'm a bit stuck on.<br>        
+        - Highscores board for all three modes coming soon.<br>        
+        - Linked through to the working app without the dropdown options on the 'setup' page requiring correct info, simply to get the app up and running again. I need to work out how I'll be creating that database and the dropdown selection to choose the correct setup for the game.`
+    },
+    {
+        version : "v1.1.2",
+        description : "Quick commit as I change from iMac to laptop"
+    },
+    {
+        version : "v1.1.1",
+        description : "Added the options page with styling. Refactoring CSS as I go using utility classes more."
+    },
+    {
+        version : "v1.1.0",
+        description : "Started work on multiscreen app with menus and navigation to new quiz types."
+    },
+    {
+        version : "v1.0.6",
+        description : "Added site.webmanifest and related icons and favicons to create a progressive web app"
+    },
+    {
+        version : "v1.0.5",
+        description : "Slight tweak to make the score and question-total update simultaneously"
+    },
+    {
+        version : "v1.0.4",
+        description : "Added a very primative scoring system. Added a logo in the masthead. Tidied up the app with more CSS. Added a footer with credit line."
+    },
+    {
+        version : "v1.0.3",
+        description : "Added keyboard 'enter' key support for submitting answers"
+    },
+    {
+        version : "v1.0.2",
+        description : "Change input field to number"
+    },
+    {
+        version : "v1.0.1",
+        description : "Updated entire Sister of Battle Index to the database"
+    },
+    {
+        version : "v1.0",
+        description : "The app is working! Enough to but it into beta v1.0 anyway. The questions cycle though all units and all stats. Next job is to add more unit and stat content."
+    },
+    {
+        version : "v0.1.3",
+        description : "CSS style changes, added popup score message with setTimeout() function. Merge branch 'main' of https://github.com/thewavyblue/Mindforge-40k"
+    },
+    {
+        version : "v0.1.2",
+        description : "Added CSS and some HTML. Create README.md"
+    },
+    {
+        version : "v0.1.1",
+        description : "Checking input value against stats. Maths now adds up!"
+    },
+    {
+        version : "v0.1",
+        description : "Bare-bone of the app! Its kinda working though...sorta"
+    }
 ];
-
